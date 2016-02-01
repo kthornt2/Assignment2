4e533447f56e7bf3680422a5f2f4960c226af8b2
Kyle Thornton <kthornt2@oakland.edu>
2016-02-01 17:27:56 -0500

    Added tested code to Ninenty_Nine_Bottles methods

--- BeerSong.java
+++ BeerSong.java
@@ -1,12 +1,31 @@
 public class BeerSong {
 
     public static void main(String[] args) {
-        
+       
+
     }
 
 
     public static void Ninety_Nine_Bottles(){
-        
+        int beers=99;
+        String name="bottles";
+        while(beers>1){
+            System.out.println();
+            System.out.println(beers + " " + name + " " + "of beer on the wall," + beers + " " + name + " " +"of beer," );
+            beers--;
+            if(beers>1)
+            {
+                System.out.println("take one down,pass it around," + beers + " " + name + " " + "of beer on the wall.");
+            }
+            else
+            {
+                name = "bottle";
+                System.out.println("take one down,pass it around," + beers + " " + name + " " + "of beer on the wall");
+            }
+        }
+        System.out.println();
+        System.out.println(beers + " " + name + " " + "of beer on the wall," + beers + " " + name + " " + "of beer," );
+        System.out.println("take one down,pass it around," + "no more bottles of beer on the wall");
 
     }
 }
